6863702f4929c7c3b5b39f8181324b90de25abc1
Adam Azad <adam@adamazad.com>
2020-11-14 19:19:53 +0300

    Add specs

---
+++ src/services/accounts/index.spec.ts
@@ -0,0 +1,87 @@
+import Mongoose from 'mongoose';
+import { EmailAlreadyInUseError } from './AccountError';
+import * as AccountService from '.';
+import UserModel from '@models/User';
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/jest';
+
+beforeAll(async () => {
+  await Mongoose.connect(MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  }).catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+});
+
+beforeEach(async () => {
+  await UserModel.deleteMany({});
+});
+
+afterAll(async () => {
+  await Mongoose.connection.close();
+});
+
+describe('Auth Service', () => {
+  const testUser = {
+    name: 'Test',
+    email: 'test@test.com',
+    password: 'test',
+  };
+
+  describe('register', () => {
+    test('Should register a new user with email and password', async () => {
+      await AccountService.registerWithEmailAndPassword(
+        testUser.email,
+        testUser.password
+      );
+
+      const account = await AccountService.findAccountByEmail(testUser.email);
+
+      expect(account?.email).toMatch(testUser.email);
+    });
+
+    test('Should fail creating an account with duplicate email', async () => {
+      // Create user
+      await AccountService.registerWithEmailAndPassword(
+        testUser.email,
+        testUser.password
+      );
+
+      const failedToCreate = AccountService.registerWithEmailAndPassword(
+        testUser.email,
+        testUser.password
+      );
+
+      expect(failedToCreate).rejects.toThrow(EmailAlreadyInUseError);
+    });
+  });
+
+  describe('login', () => {
+    test('Should log in a user with right credentials', async () => {
+      await AccountService.registerWithEmailAndPassword(
+        testUser.email,
+        testUser.password
+      );
+
+      const appUser = await AccountService.loginWithEmailAndPassword(
+        testUser.email,
+        testUser.password
+      );
+
+      expect(appUser.email).toEqual(testUser.email);
+    });
+  });
+
+  test('Should retrieve one user from database by email', async () => {
+    await AccountService.registerWithEmailAndPassword(
+      testUser.email,
+      testUser.password
+    );
+
+    const user = await AccountService.findAccountByEmail(testUser.email);
+    expect(user?.email).toEqual(testUser.email);
+  });
+});
---
+++ src/services/messages/index.spec.ts
@@ -0,0 +1,114 @@
+import Mongoose from 'mongoose';
+import MessageModel from '@models/Message';
+import * as MessagesService from '.';
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/jest';
+
+beforeAll(async () => {
+  await Mongoose.connect(MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  }).catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+});
+
+beforeEach(async () => {
+  await MessageModel.deleteMany({});
+});
+
+afterAll(async () => {
+  await Mongoose.connection.close();
+});
+
+describe('Messages Service', () => {
+  describe('Create', () => {
+    test('Should create a new message', async () => {
+      const testMessage = {
+        user: '5fafa23b3634753c0877eab4',
+        message: 'Jesting it',
+      };
+
+      const newMessage = await MessagesService.createMessage(testMessage);
+
+      const foundMessage = await MessagesService.findMessageById(newMessage.id);
+
+      expect(foundMessage?.user.toString()).toMatch(testMessage.user);
+      expect(foundMessage?.message).toMatch(testMessage.message);
+    });
+
+    test('Should create a reply', async () => {
+      const testMessage = {
+        user: '5fafa23b3634753c0877eab4',
+        message: 'Jesting it',
+      };
+
+      const post = await MessagesService.createMessage(testMessage);
+
+      const newReply = await MessagesService.createMessage({
+        ...testMessage,
+        parentId: post._id,
+      });
+
+      expect(newReply.parentId?.toString()).toEqual(post.id.toString());
+      expect(newReply.message).toEqual(testMessage.message);
+      expect(newReply.user.toString()).toEqual(testMessage.user);
+    });
+  });
+
+  describe('List', () => {
+    test('List a collection of messages', async () => {
+      const mockMessages = [
+        {
+          user: '5fafa23b3634753c0877eab4',
+          message: 'Message #1',
+        },
+        {
+          user: '5fafa23b3634753c0877eab4',
+          message: 'Message #2',
+        },
+        {
+          user: '5fafa23b3634753c0877eab4',
+          message: 'Message #3',
+        },
+      ];
+
+      for (let mockMessage of mockMessages) {
+        await MessagesService.createMessage(mockMessage);
+      }
+
+      const foundMessages = await MessagesService.findMessages();
+      // type of Array
+      expect(Array.isArray(foundMessages)).toBeTruthy();
+      expect(foundMessages.length).toEqual(mockMessages.length);
+      // Compare submitted messages agaisnt created ones
+      const contentExists = foundMessages.every(
+        foundMessage =>
+          mockMessages.find(
+            mockMessage => mockMessage.message === foundMessage.message
+          ) != undefined
+      );
+      expect(contentExists).toBeTruthy();
+    });
+
+    test('Should list message by id', async () => {
+      const testMessage = {
+        user: '5fafa23b3634753c0877eab4',
+        message: 'Jesting it',
+      };
+
+      const post = await MessagesService.createMessage(testMessage);
+
+      const newReply = await MessagesService.createMessage({
+        ...testMessage,
+        parentId: post._id,
+      });
+
+      expect(newReply.parentId?.toString()).toEqual(post._id.toString());
+      expect(newReply.message).toEqual(testMessage.message);
+      expect(newReply.user.toString()).toEqual(testMessage.user);
+    });
+  });
+});
