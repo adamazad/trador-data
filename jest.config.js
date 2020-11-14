f2782529ba3b96402b618f0bd9e96055a2e1ea86
Adam Azad <adam@adamazad.com>
2020-11-14 19:19:41 +0300

    Add Jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  roots: ['<rootDir>'],
+  testEnvironment: 'node',
+  setupFiles: [],
+  setupFilesAfterEnv: [],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+  testTimeout: 30000,
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src',
+  }),
+};
