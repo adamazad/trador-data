d6488daf6d27b3dbba493c635457116ce6dc2952
Adam Azad <adam@adamazad.com>
2020-11-22 20:37:33 +0300

    Add basic model checks (WIP)

---
+++ src/models/Message.spec.ts
@@ -0,0 +1,7 @@
+import MessageModel from './Message';
+
+describe('Message Model', () => {
+  test('It should be defined', () => {
+    expect(MessageModel).toBeDefined();
+  });
+});
---
+++ src/models/User.spec.ts
@@ -0,0 +1,7 @@
+import UserModel from './User';
+
+describe('Message Model', () => {
+  test('It should be defined', () => {
+    expect(UserModel).toBeDefined();
+  });
+});
