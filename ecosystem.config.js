544e5f3754a2350a2da7258e51e6de6729f08945
Adam Azad <adam@adamazad.com>
2020-11-22 20:38:00 +0300

    Shared object for common props

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,36 +1,28 @@
-const namespace = 'trador';
+const sharedAppConfig = {
+  namespace: 'trador',
+  instances: 2,
+  exec_mode: 'cluster',
+  env: {
+    NODE_ENV: 'development',
+  },
+  env_production: {
+    NODE_ENV: 'production',
+  },
+  node_args: '-r ts-node/register -r tsconfig-paths/register',
+  args: '--color',
+};
 
 module.exports = {
   apps: [
     {
-      namespace,
+      ...sharedAppConfig,
       name: 'trador-api-servers',
       script: './build/servers/api-server/index.js',
-      instances: 2,
-      exec_mode: 'cluster',
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
-      node_args: '-r ts-node/register -r tsconfig-paths/register',
-      args: '--color',
     },
     {
-      namespace,
+      ...sharedAppConfig,
       name: 'trador-auth-server',
       script: './build/servers/auth-server/index.js',
-      instances: 2,
-      exec_mode: 'cluster',
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
-      node_args: '-r ts-node/register -r tsconfig-paths/register',
-      args: '--color',
     },
   ],
 };
